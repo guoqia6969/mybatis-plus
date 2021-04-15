8d8271d89396a14f4bc446062e52083dd7ae0694
王帅豪 <wsh82898@163.com>
2021-04-15 19:41:39 +0800

    测试增删改查

--- src/test/java/com/atguigu/mybatisplus/MybatisPlusApplicationTests.java
+++ src/test/java/com/atguigu/mybatisplus/MybatisPlusApplicationTests.java
@@ -6,7 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class MybatisPlusApplicationTests {
@@ -22,5 +26,52 @@ class MybatisPlusApplicationTests {
         List<User> users = userMapper.selectList(null);
         users.forEach(System.out::println);
     }
+
+    @Test
+    void testInsert(){
+        User user = new User();
+        user.setName("王大炮");
+        user.setAge(22);
+        user.setEmail("wsh82898@163.com");
+        int result = userMapper.insert(user);
+        System.out.println("影响的行数"+result);
+        System.out.println("id："+user.getId());
+    }
+
+
+    @Test
+    void testSelect(){
+        //根据id查询单个
+        User user = userMapper.selectById(1L);
+        System.out.println("user = " + user);
+        System.out.println("---------------------------");
+        //根据ids查询多个
+        List<User> users = userMapper.selectBatchIds(Arrays.asList(1, 2, 3, 4));
+        users.forEach(System.out::println);
+        System.out.println("---------------------------");
+        //根据多个条件查询
+        Map<String,Object> map = new HashMap<>();
+        map.put("name","王大炮");
+        map.put("age",22);
+        List<User> users1 = userMapper.selectByMap(map);
+        users1.forEach(System.out::println);
+    }
+
+    @Test
+    void testUpdate(){
+        User user = new User();
+        user.setId(5L);
+        user.setName("阿拉蕾");
+        int result = userMapper.updateById(user);
+        System.out.println("本次更新记录："+result);
+    }
+
+    @Test
+    void testDelete(){
+        int i = userMapper.deleteById(1);
+        System.out.println("本次删除条数："+i);
+    }
+
+
 
 }
