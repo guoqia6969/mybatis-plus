79a6ee76dd231f2f57f48ea96d782c5465522fe8
王帅豪 <wsh82898@163.com>
2021-04-19 18:50:15 +0800

    User常用注解

--- src/main/java/com/atguigu/mybatisplus/entity/User.java
+++ src/main/java/com/atguigu/mybatisplus/entity/User.java
@@ -1,5 +1,7 @@
 package com.atguigu.mybatisplus.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 /**
@@ -9,10 +11,15 @@ import lombok.Data;
  * @Date 2021/4/14 19:20
  */
 @Data
+@TableName(value = "t_user")
 public class User {
     /**
      * 主键id
+     * 1@TableId 告诉mybatisPlus 这个字段是这个表的主键ID
+     * mybatisPLus默认使用雪花算法id生成策略
+     * 2@TableId (type = IdType.ASSIGN_ID)
      */
+    @TableId(value = "uid")
     private Long id;
 
     /**
