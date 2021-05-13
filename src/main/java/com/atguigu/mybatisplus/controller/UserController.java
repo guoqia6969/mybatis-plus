b9dd2aa6afb90c694ca8fad7a2183e1612b17de6
王帅豪 <wsh82898@163.com>
2021-05-13 19:03:05 +0800

    增加查询用户列表接口

---
+++ src/main/java/com/atguigu/mybatisplus/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.atguigu.mybatisplus.controller;
+
+import com.atguigu.mybatisplus.entity.User;
+import com.atguigu.mybatisplus.service.UserService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author 24787
+ * @author wangsh
+ * @Classname UserController
+ * @Description
+ * @Date 2021/5/11 19:13
+ * @ CrossOrigin 允许跨域
+ */
+@RestController
+@RequestMapping("/user")
+@CrossOrigin
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @GetMapping("/list")
+    private List<User> list(){
+        return userService.list();
+    }
+}
